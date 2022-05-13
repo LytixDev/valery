a7ed6ebeffecdf8f13f66101974d8fb3beea0a54
Nicolai Brand <nicolaihbrand@gmail.com>
2022-05-13 19:39:43 +0200

    enhance: Dynamic reading of input buffer (#5)

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ mkdir -p "$HOME/.local/share/valery/bin"
 bin_dir="$HOME/.local/share/valery/bin"
 
 gcc -o bin/which bin/which.c
-gcc -o valery valery.c
+gcc -o valery valery.c utils/prompt.c
 
 cp bin/which $bin_dir

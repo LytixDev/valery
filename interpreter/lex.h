7dd5c1a68d18369212708f1ebe8c8c62be482b9b
Nicolai Brand <nicolahb@stud.ntnu.no>
2022-09-29 21:37:31 +0200

    skeleton

---
+++ interpreter/lex.h
@@ -0,0 +1,34 @@
+/*
+ *  Copyright (C) 2022 Nicolai Brand 
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef VALERY_INTERPRETER_LEX_H
+#define VALERY_INTERPRETER_LEX_H
+
+
+/* types */
+typedef struct token_t Token;
+typedef struct lex_t Lex;
+
+
+/* functions */
+Lex *tokenize(char *source);
+
+void lex_free(Lex *lx);
+
+void lex_dump(Lex *lx);
+
+
+#endif /* !VALERY_INTERPRETER_LEX_H */
